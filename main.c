9cf639d8553ae4f6df22c1752cf42612c905a9f6
Mak0169 <mak.gumpf@gmail.com>
2025-12-26 16:27:52 -0500

    Changed the variables and types have to create a file reader in the main

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    TOKEN_IDENTIFER,
+    TOKEN_KEYWORD,
+    TOKEN_NUMBER,
+    TOKEN_STRING_LITERAL,
+    TOKEN_SEPARATOR
+} tokenType;
+
+typedef enum {
+    SEMI,
+    OPEN_PAREN,
+    CLOSE_PAREN,
+    OPEN_BRACK,
+    CLOSE_BRAK
+} tokenSeperator;
+
+typedef enum {
+    IF,
+    ELSE,
+    WHILE,
+    FOR,
+    RETURN,
+    INT,
+    FLOAT
+} tokenKeyword;
 
 typedef struct {
+    TokenType type; 
+} Token;
 
-} tokenIdentifier;
-
-typedef enum {
-
-} tokenKeyWord;
-
-typedef enum {
-    
-} tokenSeperator;
 
 int main() {
     
