bacde6a8c6e1e69a9366d76bc6014e19111f3e44
Mak0169 <mak.gumpf@gmail.com>
2026-01-26 21:47:53 -0500

    moved the enums to a different file. Also, changed some of the constants.

--- main.c
+++ main.c
@@ -1,38 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum {
-    TOKEN_IDENTIFER,
-    TOKEN_KEYWORD,
-    TOKEN_NUMBER,
-    TOKEN_STRING_LITERAL,
-    TOKEN_SEPARATOR
-} tokenType;
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s\n", argv[0]);
+        return 1;
+    }
 
-typedef enum {
-    SEMI,
-    OPEN_PAREN,
-    CLOSE_PAREN,
-    OPEN_BRACK,
-    CLOSE_BRAK
-} tokenSeperator;
+    char *src = read_file(argc[1]);
 
-typedef enum {
-    IF,
-    ELSE,
-    WHILE,
-    FOR,
-    RETURN,
-    INT,
-    FLOAT
-} tokenKeyword;
-
-typedef struct {
-    TokenType type; 
-} Token;
-
-
-int main() {
-    
 }
 
