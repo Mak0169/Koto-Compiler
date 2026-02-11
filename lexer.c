992bc76fadf7e6fa2b88c5ec9d0b1113e4665917
Mak0169 <mak.gumpf@gmail.com>
2026-02-10 19:45:05 -0500

    finished next_token function and going to start token_print

--- lexer.c
+++ lexer.c
@@ -4,4 +4,23 @@
 #inlcude <string.h>
 #inlcude <ctype.h>
 
-int next_token(FILE *fileOp, Token *tok) {}
+int next_token(FILE *fileOp, Token *tok) {
+    int c;
+    int lineCount = 0;
+    
+    while ((c = fgetc(fp)) != EOF) {
+        if (c == '\n') {
+             lineCount++;
+        }
+        if (c == ' ' | c == '\t') {
+            continue;
+        }
+        break;
+    }
+
+    if (c == EOF) {
+        tok->type = TOKEN_EOF;
+        return 0;
+    }
+    return 1;
+}
