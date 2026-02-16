f4da2a828d2a43cc32b44acea36d083d466cff74
Mak0169 <mak.gumpf@gmail.com>
2026-02-16 10:33:34 -0500

    fixed typo

--- src/lexer.c
+++ src/lexer.c
@@ -1,8 +1,8 @@
 #include "lexer.h"
 #include <stdio.h>
 #include <stdlib.h>
-#inlcude <string.h>
-#inlcude <ctype.h>
+#include <string.h>
+#include <ctype.h>
 
 int next_token(FILE *fileOp, Token *tok) {
     int c;
